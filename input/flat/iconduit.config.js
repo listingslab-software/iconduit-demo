578a1712f39968d1f3e1e5aa616e079b407fe064
Erin Millard <ezzatron@gmail.com>
2019-05-26 20:35:02 +1000

    Add a shortName for each app

--- input/flat/iconduit.config.js
+++ input/flat/iconduit.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   name: 'Iconduit (Flat)',
+  shortName: 'Iconduit',
   description: 'The Iconduit brand in flat style.',
   colors: {
     brand: '#D5415C',
--- input/material/iconduit.config.js
+++ input/material/iconduit.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   name: 'Iconduit (Material)',
+  shortName: 'Iconduit',
   description: 'The Iconduit brand in "Material Design" style.',
   colors: {
     brand: '#D5415C',
