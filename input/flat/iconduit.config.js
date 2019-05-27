6b3c2039bea8c49733f6180272b2bce63e167a99
Erin Millard <ezzatron@gmail.com>
2019-05-27 11:07:35 +1000

    Update input assets

--- input/flat/iconduit.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'Iconduit (Flat)',
-  shortName: 'Iconduit',
-  description: 'The Iconduit brand in flat style.',
-  colors: {
-    brand: '#D5415C',
-  },
-  outputPath: '../../dist/flat',
-}
--- input/material/iconduit.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'Iconduit (Material)',
-  shortName: 'Iconduit',
-  description: 'The Iconduit brand in "Material Design" style.',
-  colors: {
-    brand: '#D5415C',
-  },
-  outputPath: '../../dist/material',
-}
